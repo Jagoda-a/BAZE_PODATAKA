07cfa45d9cfb9f3961d4915ae2f2acb4e5412bf6
Pedja1201 <radakpredrag10@gmail.com>
2020-12-03 18:24:23 +0100

    Izmene workspace

--- Qt dizajn/workspace.py
+++ Qt dizajn/workspace.py
@@ -15,16 +15,16 @@ class WorkspaceWidget(QtWidgets.QWidget):
 
 
 
-    # def show_tabs(self):
-    #     self.tab_widget.addTab(QtWidgets.QTextEdit(self.tab_widget), QtGui.QIcon("picture/tabela.png"), "Prva Podtabela")
+    def show_tabs(self):
+        self.tab_widget.addTab(QtWidgets.QTextEdit(self.tab_widget), QtGui.QIcon("picture/tabela.png"), "Prva Podtabela")
 
-    #     self.tab_widget.addTab(QtWidgets.QTextEdit(self.tab_widget), QtGui.QIcon("picture/tabela.png"), "Druga Podtabela")
+        self.tab_widget.addTab(QtWidgets.QTextEdit(self.tab_widget), QtGui.QIcon("picture/tabela.png"), "Druga Podtabela")
 
     def create_tab_widget(self):
         self.tab_widget = QtWidgets.QTabWidget(self)
         self.tab_widget.setTabsClosable(True)
         self.tab_widget.tabCloseRequested.connect(self.delete_tab)
-        # self.show_tabs()
+        self.show_tabs()
 
     def delete_tab(self, index):
         self.tab_widget.removeTab(index)
