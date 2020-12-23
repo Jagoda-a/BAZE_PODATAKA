0d6a84e3ee20624112aa05ce213fc323b0698a66
Jagoda-a <jagoda.jagodic99@gmail.com>
2020-12-23 05:41:32 -0800

    izmena main-a dodavanje toolbara, i dodela funkcija actionima

--- Qt dizajn/main_window.py
+++ Qt dizajn/main_window.py
@@ -1,6 +1,8 @@
 import sys
-from PySide2 import QtWidgets, QtGui, QtCore
+from PySide2 import QtWidgets, QtGui, QtCore, QtPrintSupport
+from PySide2.QtWidgets import *
 from PySide2.QtGui import *
+from PySide2.QtCore import *
 from structure_dock import StructureDock
 from workspace import WorkspaceWidget
 from workspace import *
@@ -13,16 +15,10 @@ class MainWindow(QtWidgets.QMainWindow):
         self.main_window.setWindowTitle("RJP BASE")
         self.main_window.resize(880, 580)
         self.main_window.setWindowIcon(QIcon("RJP.png"))
-        self.main_window.setStyleSheet('background-color:#222; color:grey')
+        self.main_window.setStyleSheet('color:black')
         
-        
-        
-        
-
         self.create_menu()
 
-
-        
         self.main_window.show()
         
 
@@ -39,17 +35,21 @@ class MainWindow(QtWidgets.QMainWindow):
 
         open_action=QtWidgets.QAction(QIcon("./picture/openfile.png"), "Open File", self)
         open_action.setShortcut('Ctrl+O')
+        open_action.triggered.connect(self.open_file)
 
         open_folder=QtWidgets.QAction(QIcon("./picture/open.png"), "Open Folder", self)
         open_folder.setShortcut('Ctrl+K+O')
 
         save_action=QtWidgets.QAction(QIcon('./picture/save.png'), "Save", self)
         save_action.setShortcut('Ctrl+S')
+        save_action.triggered.connect(self.save_file)
 
         print_action=QtWidgets.QAction(QIcon('./picture/pp.png'),"Print",self)
         print_action.setShortcut('Ctrl+P')
+        print_action.triggered.connect(self.print_dialog)
 
         exit_action=QtWidgets.QAction(QIcon('./picture/xx.png'), "Exit", self)
+        exit_action.triggered.connect(self.exit_app)
 
         file_menu.addAction(new_action)
         file_menu.addAction(open_action)
@@ -96,30 +96,30 @@ class MainWindow(QtWidgets.QMainWindow):
         self.menu_bar.addMenu(help_menu)
 
         self.main_window.setMenuBar(self.menu_bar)
+        self.menu_bar.setStyleSheet('background-color:#222; color:white;')
         
         #STRUCTURE DOCK
-        structure_dock=StructureDock("Strukture dokumenta", self.main_window)
-        structure_dock.setStyleSheet("color:white")
+        structure_dock=StructureDock("Strukture dokumenata", self.main_window)
+        structure_dock.setStyleSheet("background-color:#222; color:white")
+
+        #TOOLBAR
+        self.toolbar=QtWidgets.QToolBar(self.main_window)
+        self.main_window.addToolBar(self.toolbar)
+        self.toolbar.addAction(new_action)
+        self.toolbar.addAction(open_action)
+        self.toolbar.addAction(save_action)
+        self.toolbar.addAction(print_action)
 
         
-
-
         #CENTRALNI VIDZET
         
         central_widget=QtWidgets.QTabWidget(self.main_window)
-        central_widget.setStyleSheet('backg')
         central_widget.setTabsClosable(True)
 
         def delete_tab(index):
             central_widget.removeTab(index)
         central_widget.tabCloseRequested.connect(delete_tab)
         
-        
-
-
-        
-
-
         #WORKSPACE
         workspace=WorkspaceWidget(central_widget)
         central_widget.addTab(workspace,QtGui.QIcon("picture/tabela.png"), "Prikaz tabele")
@@ -136,6 +136,34 @@ class MainWindow(QtWidgets.QMainWindow):
         structure_dock.tree.clicked.connect(read_file)
         self.main_window.setCentralWidget(central_widget)
         self.main_window.addDockWidget(QtCore.Qt.LeftDockWidgetArea, structure_dock)
+
+    def save_file(self):
+        fn, _ = QFileDialog.getSaveFileName(self, "Save File", None, "PDF files (.pdf); All files")
+
+        if fn != '':
+
+            if QFileInfo(fn).suffix() == "": fn += '.pdf'
+
+            printer = QtPrintSupport.QPrinter(QtPrintSupport.QPrinter.HighResolution)
+            printer.setOutputFormat(QtPrintSupport.QPrinter.PdfFormat)
+            printer.setOutputFileName(fn)
+        self.workspace.document().print_(self.printer)
+    
+    def print_dialog(self):
+        printer = QtPrintSupport.QPrinter(QtPrintSupport.QPrinter.HighResolution)
+        dialog = QtPrintSupport.QPrintDialog(printer, self)
+
+        if dialog.exec_() == QtPrintSupport.QPrintDialog.Accepted:
+            self.textEdit.print_(printer)
+
+    def exit_app(self):
+        self.main_window.close()
+
+    def open_file(self):
+            path_to_file, _ = QFileDialog.getOpenFileName(self, self.tr("Load File"), self.tr("~/Desktop/"), self.tr("PDF (.pdf)"))
+
+            self.file_viewer = self.FileViewer(path_to_file)
+            self.file_viewer.show()
 
         
 
